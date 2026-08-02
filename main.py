45fb0467433b2c5a9a2b108c59fbe7b48225b220
P N Harish kumaran <pnharishkumaran@gmail.com>
2026-08-02 22:19:27 +0530

    generating frame using opencv

--- main.py
+++ main.py
@@ -1,26 +1,31 @@
 import cv2
-x=320
-y=240
+import mediapipe as mp
+
+mp_hands=mp.solutions.hands
+hands=mp_hands.Hands()
+
+mp_draw=mp.solutions.drawing_utils
 
 camera = cv2.VideoCapture(0)
 while True:
     success, frame=camera.read()
     if not success:
         break
-    cv2.circle(frame,(x,y),50,(255,0,0),-1)
- 
-    cv2.imshow("Rasengan",frame)
-    key = cv2.waitKey(1)
-    if key == ord("a"):
-        x-=10
-    elif key == ord("d"):
-        x+=10
-    elif key == ord("s"):
-        y+=10
-    elif key == ord("w"):
-        y-=10
-    elif key == ord("q"):
-        break
+    rgb_frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+    results = hands.process(rgb_frame)
+
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            mp_draw.draw_landmarks(
+                frame,
+                hand_landmarks,
+                mp_hands.HAND_CONNECTIONS
+            )
+    cv2.imshow("Rasengan CV",frame)
+
+    if cv2.waitKey(1) == ord("q"):
+        break     
+
 
 camera.release()
 cv2.destroyAllWindows()
